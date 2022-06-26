784f8a028a4055bb1e5466dcabbe80e5063169a8
Matigakis Panagiotis <pmatigakis@gmail.com>
2022-06-26 13:43:38 +0300

    Added the lisence file, declared that the required python version must be greater that or equal to 3.8 and updated the gitignore file

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup
 
+
 setup(
-      name = "article_extraction",
-      version = "0.1",
-
-      packages = ["article_extraction"],
-
-      install_requires = ["lxml==3.5.0"],
-
-      author = "Panagiotis Matigakis",
-      author_email = "pmatigakis@gmail.com",
-      description = "Article text extraction library",
+      name="article_extraction",
+      version="0.2.0-dev",
+      packages=["article_extraction"],
+      include_package_data=True,
+      zip_safe=True,
+      python_requires=">=3.8",
+      install_requires=["lxml>=4.7.1"],
+      author="Panagiotis Matigakis",
+      author_email="pmatigakis@gmail.com",
+      description="Article text extraction library",
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            "Programming Language :: Python :: 3",
+            'Programming Language :: Python :: Implementation :: CPython',
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ]
 )
