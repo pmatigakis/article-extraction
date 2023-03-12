6a6041c0b73f71d29db70eaa7d85b18c46d92648
Matigakis Panagiotis <pmatigakis@gmail.com>
2023-03-12 19:07:57 +0200

    Cleaned up the example script

--- examples/extract_article.py
+++ examples/extract_article.py
@@ -1,18 +1,15 @@
-import codecs
-from sys import argv
+from argparse import ArgumentParser
 
 from article_extraction.mss import MSSArticleExtractor
 
-if len(argv) != 3:
-    print("You must specify a url and an output file")
-    quit()
-
-url = argv[1]
-output = argv[2]
+parser = ArgumentParser()
+parser.add_argument("output")
+parser.add_argument("url")
+args = parser.parse_args()
 
 article_extractor = MSSArticleExtractor()
 
-article = article_extractor.extract_article_from_url(url)
+article = article_extractor.extract_article_from_url(args.url)
 
-with codecs.open(output, "w", encoding="utf-8") as f:
+with open(args.output, "w") as f:
     f.write(article)
