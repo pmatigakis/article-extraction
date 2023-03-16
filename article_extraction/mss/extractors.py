dcbc0f3eb067b4ec049cf86eff30d5e594fed034
Matigakis Panagiotis <pmatigakis@gmail.com>
2023-03-16 22:10:02 +0200

    Added docstrings

--- article_extraction/mss/extractors.py
+++ article_extraction/mss/extractors.py
@@ -12,12 +12,22 @@ class MSSArticleExtractor(ArticleExtractor):
     """Extract the page article using the Maximum Subsequence algorithm."""
 
     def __init__(self, scoring: Optional[TermTypeScores] = None):
+        """Create a new MSSArticleExtractor object
+
+        :param scoring: the scoring object to use for the term score
+            calculation
+        """
         self.scoring = scoring or TermTypeScores()
 
     def _find_maximum_subsequence(
         self, scores: List[float]
     ) -> Tuple[int, int]:
-        """Find the subsequence with the highest score."""
+        """Find the subsequence with the highest score.
+
+        :param scores: the score for every token in the term sequence
+        :returns: return a tuple with the start and end index of the sequence
+            with the maximum score
+        """
         start = 0
         pos = 0
         sm = 0
@@ -38,7 +48,13 @@ class MSSArticleExtractor(ArticleExtractor):
     def _extract_maximum_subsequence(
         self, tokens: List[str], scores: List[float]
     ) -> List[str]:
-        """Return the term sequence with the highest score."""
+        """Return the term sequence with the highest score.
+
+        :param tokens: the token sequence
+        :param scores: the score for every token in the term sequence
+        :returns: return a list with the terms of the sequence with the maximum
+            score
+        """
         start, length = self._find_maximum_subsequence(scores)
 
         terms = tokens[start : start + length]  # noqa: E203
--- article_extraction/mss/scores.py
+++ article_extraction/mss/scores.py
@@ -1,9 +1,21 @@
 class TermTypeScores:
-    def __init__(self, word_score=1.0, tag_score=-2.0):
+    """Container for the term scores"""
+
+    def __init__(self, word_score: float = 1.0, tag_score: float = -2.0):
+        """Create a new TermTypeScores object
+
+        :param word_score: the score for words
+        :param tag_score: the score for html tags
+        """
         self.word_score = word_score
         self.tag_score = tag_score
 
     def score(self, term: str) -> float:
+        """Calculate the score for the given term
+
+        :param term: the term for which we want to calculate the score
+        :return: returns the calculated score
+        """
         if term.startswith("<") and term.endswith(">"):
             return self.tag_score
         else:
